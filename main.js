a08874172a6901d69e655da01d8ab3ad1bb2a610
sC0der <102285034+SGc0der@users.noreply.github.com>
2023-06-19 10:39:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 // https://teachablemachine.withgoogle.com/models/_G0YusIQD/
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/_G0YusIQD/model.json", modelReady);
+}
+function modelReady() {
+    classifier.classify(gotResults);
+}
+function gotResults(error, results) {
+    console.log("got result");
+}
